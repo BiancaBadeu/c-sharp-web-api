e1d80daa3a4675c9b9e37f96edc12422a2ca9e66
Bianca Cristina Badeu <92147004+BiancaBadeu@users.noreply.github.com>
2025-10-26 13:53:25 +0100

    set up connection to postgresql db

--- Models/TodoItemDTO.cs
+++ Models/TodoItemDTO.cs
@@ -5,4 +5,6 @@ public class TodoItemDTO
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime? Updated { get; set; }
 }
