77ac0eb0dd983f4a30789c8dcaa8fcb24620aaf0
Bianca Cristina Badeu <92147004+BiancaBadeu@users.noreply.github.com>
2025-10-27 15:02:07 +0100

    updated model, tested api connection to postman and connected postgresql db

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -1,9 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace TodoApi.Models;
 
 public class TodoItem
 {
+    [Key]
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
     public string? Secret { get; set; }
+    public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+    public bool Synchronized { get; set; } = false;
 }
--- Models/TodoItemDTO.cs
+++ Models/TodoItemDTO.cs
@@ -5,4 +5,7 @@ public class TodoItemDTO
     public long Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
+    public DateTime CreatedAt { get; set; } =  DateTime.UtcNow;
+    public DateTime? UpdatedAt { get; set; } = DateTime.UtcNow;
+    public bool Synchronized { get; set; } = false;
 }
